e233ebc6edcb8c6fc692bc8d732107b9797e9412
Ismail Hossen <ihossen016@gmail.com>
2021-09-09 12:18:52 +0600

    Design Completed

---
+++ main.js
@@ -0,0 +1,88 @@
+// On/Off Button Functions
+function btn1() {
+  // Color Toggle
+  let element = document.getElementById('btn1')
+  element.classList.toggle('toggle')
+
+  // Text Toggle
+  if (element.innerHTML === 'Off') {
+    element.innerHTML = 'On'
+  } else {
+    element.innerHTML = 'Off'
+  }
+}
+function btn2() {
+  // Color Toggle
+  let element = document.getElementById('btn2')
+  element.classList.toggle('toggle')
+
+  // Text Toggle
+  if (element.innerHTML === 'Off') {
+    element.innerHTML = 'On'
+  } else {
+    element.innerHTML = 'Off'
+  }
+}
+function btn3() {
+  // Color Toggle
+  let element = document.getElementById('btn3')
+  element.classList.toggle('toggle')
+
+  // Text Toggle
+  if (element.innerHTML === 'Off') {
+    element.innerHTML = 'On'
+  } else {
+    element.innerHTML = 'Off'
+  }
+}
+function btn4() {
+  // Color Toggle
+  let element = document.getElementById('btn4')
+  element.classList.toggle('toggle')
+
+  // Text Toggle
+  if (element.innerHTML === 'Off') {
+    element.innerHTML = 'On'
+  } else {
+    element.innerHTML = 'Off'
+  }
+}
+function btn5() {
+  // Color Toggle
+  let element = document.getElementById('btn5')
+  element.classList.toggle('toggle')
+
+  // Text Toggle
+  if (element.innerHTML === 'Off') {
+    element.innerHTML = 'On'
+  } else {
+    element.innerHTML = 'Off'
+  }
+}
+
+// Led Button Functions
+function led1() {
+  // Color Toggle
+  let element = document.getElementById('led1')
+  element.classList.toggle('toggle')
+}
+function led2() {
+  // Color Toggle
+  let element = document.getElementById('led2')
+  element.classList.toggle('toggle')
+}
+function led3() {
+  // Color Toggle
+  let element = document.getElementById('led3')
+  element.classList.toggle('toggle')
+}
+function led4() {
+  // Color Toggle
+  let element = document.getElementById('led4')
+  element.classList.toggle('toggle')
+}
+function led5() {
+  // Color Toggle
+  let element = document.getElementById('led5')
+  element.classList.toggle('toggle')
+}
